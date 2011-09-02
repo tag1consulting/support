697eee193af50f3608bf8138698c2777861a5af8
Brandon Bergren <bdragon@rtk0.net>
2011-09-02 12:19:29 -0500

    Fix #1185544 for Drupal 7: Port support_reference.

--- support_reference/support_reference.js
+++ support_reference/support_reference.js
@@ -3,25 +3,29 @@
  * Behaviors for support_reference.
  * Based on code from api.module.
  */
-
-Drupal.behaviors.supportReferenceAutoComplete = function (context) {
-  $('#support-reference-block-form:not(.supportReferenceAutoCompleteProcessed)', context).addClass('supportReferenceAutoCompleteProcessed').each(function () {
-    // On the first focus.
-    $('#edit-reference', this).attr('autocomplete', 'off').one('focus', function () {
-      var $this = $(this);
-      // Fetch the ticket list.
-      $.getJSON(Drupal.settings.supportReferenceAutoCompletePath, function (data) {
-        // Attach to autocomplete.
-        $this.autocomplete(data, {
-          matchContains: true,
-          max: 200,
-          scroll: true,
-          scrollHeight: 360,
-          width: 300
-        }).result(function () {
-          $this.get(0).form.submit();
-        }).focus();
+(function($) {
+Drupal.behaviors.supportReferenceAutoComplete = {
+  attach: function (context) {
+    $('#support-reference-block-form:not(.supportReferenceAutoCompleteProcessed)', context).addClass('supportReferenceAutoCompleteProcessed').each(function () {
+      // On the first focus.
+      $('#edit-reference', this).attr('autocomplete', 'off').one('focus', function () {
+        var $this = $(this);
+        // Fetch the ticket list.
+        $.getJSON(Drupal.settings.supportReferenceAutoCompletePath, function (data) {
+          // Attach to autocomplete.
+          $this.autocomplete({
+            source: data,
+            matchContains: true,
+            max: 200,
+            scroll: true,
+            scrollHeight: 360,
+            width: 300
+          }).result(function () {
+            $this.get(0).form.submit();
+          }).focus();
+        });
       });
     });
-  });
+  }
 };
+})(jQuery);

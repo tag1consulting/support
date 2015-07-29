fd1eb717b9256a08c6cb2719eabce2afee31e31b
Fabian Franz <github@fabian-franz.de>
2015-07-29 14:55:30 +0200

    Fix manual test runners.

--- tests/run-behat.sh
+++ tests/run-behat.sh
@@ -7,5 +7,16 @@ set -e
 DIR=$(dirname $0)
 cd $DIR
 cd ./behat
-test -f "./vendor/bin/behat" || composer install --no-interaction --prefer-source --dev
+test -f "./vendor/bin/behat" || composer install --no-interaction --prefer-source
+
+# Checking for Phantom JS or selenium (Linux only)
+if ! { netstat -l -n | grep -q :4444; }
+then
+	echo "Error: PhantomJS or selenium could not be found." 1>&2
+	echo "" 1>&2
+	echo "Start phantomjs with:" 1>&2
+	echo "  phantomjs --webdriver=4444 &" 1>&2
+	exit 1
+fi
+
 ./vendor/bin/behat "$@"
--- tests/run-simpletest.sh
+++ tests/run-simpletest.sh
@@ -9,4 +9,11 @@ DIR=$(dirname $0)
 cd $DIR
 
 drush -y en simpletest support
-drush test-run "support" "$@"
+
+export CORE_DIR=$(drush drupal-directory)
+
+cd $CORE_DIR
+{ php ./core/scripts/run-tests.sh --color --verbose --url 'http://127.0.0.1/' "support" || echo "1 fails"; } | tee /tmp/simpletest-result.txt
+
+egrep -i "([1-9]+ fail)|(Fatal error)|([1-9]+ exception)" /tmp/simpletest-result.txt && exit 1
+exit 0

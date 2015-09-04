378a2b7f56b1a4c76eb3019113b4df0da42cd12e
Brandon Bergren <bdragon@rtk0.net>
2015-09-04 16:46:47 -0500

    SupportTicketViewLanguageTest

---
+++ modules/support_ticket/src/Tests/SupportTicketViewLanguageTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\support_ticket\Tests\SupportTicketViewLanguageTest.
+ */
+
+namespace Drupal\support_ticket\Tests;
+
+use Drupal\language\Entity\ConfigurableLanguage;
+
+/**
+ * Tests the support_ticket language extra field display.
+ *
+ * @group support_ticket
+ */
+class SupportTicketViewLanguageTest extends SupportTicketTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('support_ticket', 'datetime', 'language');
+
+  protected function setUp() {
+    parent::setUp();
+
+    $web_user = $this->drupalCreateUser(array('access support tickets'));
+    $this->drupalLogin($web_user);
+  }
+
+  /**
+   * Tests the language extra field display.
+   */
+  public function testViewLanguage() {
+    // Add Spanish language.
+    ConfigurableLanguage::createFromLangcode('es')->save();
+
+    // Set language field visible.
+    entity_get_display('support_ticket', 'ticket', 'default')
+      ->setComponent('langcode')
+      ->save();
+
+    // Create a support_ticket in Spanish.
+    $support_ticket = $this->drupalCreateSupportTicket(array('langcode' => 'es'));
+
+    $this->drupalGet($support_ticket->urlInfo());
+    $this->assertText('Spanish','The language field is displayed properly.');
+  }
+
+}
